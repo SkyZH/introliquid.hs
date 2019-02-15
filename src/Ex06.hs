40fd4c3ab2ceadd97cda15ff49b97d88569707ae
SkyZH <iskyzh@gmail.com>
2019-02-15 11:47:27 +0800

    update Ex06

--- src/Ex06.hs
+++ src/Ex06.hs
@@ -61,4 +61,68 @@ reverse xs = go [] xs where
   {-@ go :: left: List a -> right: List a -> ListN a {(size left) + (size right)} @-}
   go acc [] = acc
   go acc (x:xs) = go (x:acc) xs
-  
+
+{-@ die :: {v:String | false} -> a @-}
+die = error
+
+{-@ zipWith1 :: (a->b->c) -> xs:List a -> ListX b xs -> ListX c xs @-}
+zipWith1 f (a:as) (b:bs) = f a b : zipWith1 f as bs
+zipWith1 _ [] [] = []
+zipWith1 _ _ _ = die "no other cases"
+
+{-@ zip1 :: as:[a] -> bs:[b] -> {v:[(a, b)] | Tinier v as bs }@-}
+zip1 (a:as) (b:bs) = (a, b): zip1 as bs
+zip1 [] _ = []
+zip1 _ [] = []
+
+{-@ predicate Tinier X Y Z = Min (size X) (size Y) (size Z) @-}
+{-@ predicate Min X Y Z = (if Y < Z then X = Y else X = Z) @-}
+
+-- exercise 6.3
+{-@ zipOrNull :: xs: List a ->
+    {ys: List b | (size xs /= 0 && size ys /= 0) => (size xs == size ys)} ->
+    {v:List (a, b) | if (size xs == size ys) then (size v == size xs) else (size v == 0)} @-}
+
+zipOrNull :: [a] -> [b] -> [(a, b)]
+zipOrNull [] _ = []
+zipOrNull _ [] = []
+zipOrNull xs ys = zipWith1 (,) xs ys
+
+{-@ test1 :: {v: _ | size v = 2} @-}
+test1 = zipOrNull [0, 1] [True, False]
+
+{-@ test2 :: {v: _ | size v = 0} @-}
+test2 = zipOrNull [] [True, False]
+
+{-@ test3 :: {v: _ | size v = 0} @-}
+test3 = zipOrNull ["cat", "dog"] []
+
+
+{-@ take' :: n:Nat -> ListGE a n -> ListN a n @-}
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' n (x:xs) = x : take' (n - 1) xs
+take' _ _ = die "won't happen"
+
+{-@ type ListGE a N = {v:List a | N <= size v} @-}
+
+-- exercise 6.4
+{-@ drop' :: n:Nat -> xs:ListGE a n -> ListN a {size xs - n} @-}
+drop' :: Int -> [a] -> [a]
+drop' 0 xs = xs
+drop' n (_:xs) = drop' (n - 1) xs
+drop' _ _ = die "won't happen"
+
+{-@ test4 :: ListN String 2 @-}
+test4 = drop' 1 ["cat", "dog", "mouse"]
+
+-- exercise 6.5
+{-@ take'' :: n:Nat -> xs:List a -> ListN a {if n < (size xs) then n else (size xs)}@-}
+take'' :: Int -> [a] -> [a]
+take'' 0 _ = []
+take'' _ [] = []
+take'' n (x:xs) = x : take'' (n - 1) xs
+
+{-@ test5 :: [ListN String 2] @-}
+test5 = [ take'' 2 ["cat", "dog", "mouse"]
+        , take'' 20 ["cow", "goat"]]
